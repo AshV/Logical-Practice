3f2b72df195bc591d379756a16fe5ec986c0e974
Ashish Vishwakarma <ashish6318@gmail.com>
2017-05-14 02:03:02 +0100

    Added Solution for Sum Of Numbers Adhoc

---
+++ Sum Of Numbers Adhoc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static System.Console;
+
+namespace LogicalProblems
+{
+    class Sum_Of_Numbers_Adhoc
+    {
+        static void Main(string[] args)
+        {
+            byte T = Convert.ToByte(ReadLine());
+            long[] output = new long[T];
+            for (byte b = 0; b < T; b++)
+            {
+                int N = Convert.ToInt32(ReadLine());
+                output[b] = 0;
+                string[] array = ReadLine().Split(' ');
+                for (int i = 0; i < N; i++)
+                    output[b] += Convert.ToInt64(array[i]);
+            }
+
+            for (int i = 0; i < T; i++)
+                WriteLine(output[i]);
+        }
+    }
+}
