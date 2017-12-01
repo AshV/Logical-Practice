a1d4c6d964ac45adc0b8f38ea9f80ee1e057a026
Ashish Vishwakarma <Ashish6318@gmail.com>
2017-12-01 18:29:33 +0530

    Finding frequency

--- FindingFrequency.cs
+++ FindingFrequency.cs
@@ -2,27 +2,24 @@
 
 using System;
 
-namespace ConsoleTest
+class FindingFrequency
 {
-    class FindingFrequency
+    static void Main()
     {
-        static void Main()
+        int N = int.Parse(Console.ReadLine());
+        var strArr = Console.ReadLine().Split(' ');
+        var Arr = new int[N];
+        for (int b = 0; b < N; b++)
+            Arr[b] = int.Parse(strArr[b]);
+        int X = 0, Count = 0, Q = int.Parse(Console.ReadLine());
+        for (int i = 0; i < Q; i++)
         {
-            int N = int.Parse(Console.ReadLine());
-            var strArr = Console.ReadLine().Split(' ');
-            var Arr = new int[N];
-            for (int b = 0; b < N; b++)
-                Arr[b] = int.Parse(strArr[b]);
-            int X = 0, Count = 0, Q = int.Parse(Console.ReadLine());
-            for (int i = 0; i < Q; i++)
-            {
-                Count = 0;
-                X = int.Parse(Console.ReadLine());
-                for (int j = 0; j < N; j++)
-                    if (X == Arr[j])
-                        Count++;
-                Console.WriteLine(Count);
-            }
+            Count = 0;
+            X = int.Parse(Console.ReadLine());
+            for (int j = 0; j < N; j++)
+                if (X == Arr[j])
+                    Count++;
+            Console.WriteLine(Count);
         }
     }
 }
