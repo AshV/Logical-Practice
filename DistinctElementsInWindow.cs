26a412b8a78e40e46414a543ad41ee35349f89e2
AshV <ashish6318@gmail.com>
2017-12-09 09:16:53 +0530

    Add DistinctElementsInWindow

---
+++ DistinctElementsInWindow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using static System.Console;
+
+class DistinctElementsInWindow
+{
+    static void Main(string[] args)
+    {
+        int N, K, T = int.Parse(ReadLine());
+        for (int i = 0; i < T; i++)
+        {
+            var strInput1 = ReadLine().Split(' ');
+            N = int.Parse(strInput1[0]);
+            K = int.Parse(strInput1[1]);
+
+            var strInput2 = ReadLine().Split(' ');
+            for (int n = 0; n < N-K+1; n++)
+            {
+                var distinct = new HashSet<string>();
+                for (int k = n; k < n + K; k++)
+                    distinct.Add(strInput2[k]);
+                Write(distinct.Count + " ");
+            }
+            WriteLine();
+        }
+    }
+}
---
+++ PrintDiamondPattern.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+
+class PrintDiamondPattern
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(ReadLine());
+        for(int i=0;i<N;i++)
+        {
+            for (int j = i; j < (N/2); j++)
+                Write("0");
+            WriteLine();
+
+        }
+    }
+}
