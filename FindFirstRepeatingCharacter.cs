0c90baa4ea6b1d372658e16f7b8a8df352f125ec
AshV <ashish6318@gmail.com>
2017-12-09 10:24:45 +0530

    Add FindFirstRepeatingCharacter

---
+++ FindFirstRepeatingCharacter.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+
+class FindFirstRepeatingCharacter
+{
+    static void Main(string[] args)
+    {
+        int T = int.Parse(ReadLine());
+        for (int t = 0; t < T; t++)
+        {
+            string str = ReadLine();
+            char firstChar = '.';
+            bool flag = false;
+            for (int i = 0; i < str.Length - 1; i++)
+            {
+                firstChar = str[i];
+                flag = false;
+                for (int j = i + 1; j < str.Length; j++)
+                    if (firstChar == str[j])
+                        flag = true;
+                if (flag) break;
+            }
+            WriteLine(flag ? firstChar : '.');
+        }
+    }
+}
