3a6b958724e77df421c35686ef08fecf06af17d7
AshV <ashish6318@gmail.com>
2017-12-13 02:08:06 +0530

    Add MaximumSumOfBuildingSpeed

---
+++ MaximumSumOfBuildingSpeed.cs
@@ -0,0 +1,23 @@
+﻿// https://www.hackerearth.com/challenge/hiring/practo-developer-hiring-challenge/algorithm/maximum-sum-of-building-speed-00ab8996/
+
+using System;
+using static System.Console;
+
+class MaximumSumOfBuildingSpeed
+{
+    static void Main(string[] args)
+    {
+        int N = int.Parse(ReadLine());
+        var strArr = ReadLine().Trim().Split(' ');
+        var intArr = new int[2 * N];
+        for (int a = 0; a < 2 * N; a++)
+            intArr[a] = int.Parse(strArr[a]);
+        Array.Sort(intArr);
+        int count = 0;
+        for (int i = 0; i < 2 * N; i+=2)
+        {
+            count += intArr[i];
+        }
+        WriteLine(count);
+    }
+}
---
+++ TrailingZerosEasy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
+
+class TrailingZerosEasy
+{
+    static void Main(string[] args)
+    {
+        long N = 99999999999999, Fact = 1; ;
+        while (N > 0)
+        {
+            Fact *= N;
+            N--;
+        }
+        WriteLine(Fact);
+    }
+
+    static long Fact(long N)
+    {
+        if (N == 1)
+            return N;
+        return Fact(N - 1) * N;
+    }
+}
