05a29fbb257a0b0a7156ee96d73989cc242b21c0
AshV <ashish6318@gmail.com>
2017-12-23 13:06:03 +0530

    Added solutions

---
+++ ImplementDeQue.cs
@@ -0,0 +1,57 @@
+﻿using static System.Console;
+
+class ImplementDeQue
+{
+    static int[] deQue = new int[10000];
+    static int f = 5001, r = 5000;
+    static void Main(string[] args)
+    {
+        var T = int.Parse(ReadLine());
+        for (int t = 0; t < T; t++)
+        {
+            var str = ReadLine().Split(' ');
+            switch (str[0])
+            {
+                case "push_front":
+                    push_front(int.Parse(str[1]));
+                    break;
+                case "push_back":
+                    push_back(int.Parse(str[1]));
+                    break;
+                case "pop_front":
+                    if (f == r)
+                        WriteLine("Empty");
+                    else
+                        WriteLine(pop_front());
+                    break;
+                case "pop_back":
+                    if (f == r)
+                        WriteLine("Empty");
+                    else
+                        WriteLine(pop_back());
+                    break;
+            }
+        }
+    }
+
+    static void push_front(int e)
+    {
+        f--;
+        deQue[f] = e;
+    }
+    static void push_back(int e)
+    {
+        r++;
+        deQue[r] = e;
+    }
+    static int pop_front()
+    {
+        f++;
+        return deQue[f - 1];
+    }
+    static int pop_back()
+    {
+        r--;
+        return deQue[r + 1];
+    }
+}
---
+++ ImplementQueue.cs
@@ -0,0 +1,36 @@
+﻿using static System.Console;
+
+class ImplementQueue
+{
+    static int[] queue = new int[10000];
+    static int f = -1, r = -1;
+    static void Main(string[] args)
+    {
+        int T = int.Parse(ReadLine());
+        for (int t = 0; t < T; t++)
+        {
+            var str = ReadLine().Trim().Split(' ');
+            if (str[0] == "Enqueue")
+                Enqueue(int.Parse(str[1]));
+            else
+            {
+                if (f == r)
+                    WriteLine("Empty");
+                else
+                    WriteLine(Dequeue());
+            }
+        }
+    }
+
+    public static void Enqueue(int e)
+    {
+        r++;
+        queue[r] = e;
+    }
+
+    public static int Dequeue()
+    {
+        f++;
+        return queue[f];
+    }
+}
